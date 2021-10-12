d04ae1a5e5a96e945f17ce125021a8d708def0cf
DamianMizera <damian.mizera@student.admin.pk.edu.pl>
2021-10-12 15:31:49 +0200

    array excercise

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -1,4 +1,34 @@
-//
-// Created by damia on 11.10.2021.
-//
+#include <iostream>
+#define N 5
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 5; ++i) { //petla 5 razy wyswietla JiPP
+        cout << "JiPP" << endl;
+    }
+
+    int** tablica = new int*[N]; //tworzenie dwuwymiarowej tablicy
+    for(int i = 0; i < N; ++i) {
+        tablica[i] = new int[N];
+    }
+
+    for(int i = 0; i < N; ++i) { //przykladowe dane
+        for(int j = 0; j < N; ++j) {
+            tablica[i][j] = i + j;
+        }
+    }
+
+    for(int i = 0; i < N; i++) { //sprawdzenie
+        for(int j = 0; j < N; ++j) {
+            cout << tablica[i][j] << " ";
+        }
+    }
+
+    for(int i = 0; i < N; i++) { //zwalnianie pamieci
+        delete[] tablica[i];
+    }
+    delete[] tablica;
+
+    return 0;
+}
 
