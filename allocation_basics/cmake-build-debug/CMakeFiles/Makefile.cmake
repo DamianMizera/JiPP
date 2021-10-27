e9b0c183b4c85bb06ffea481353b08cf93682ada
DamianMizera <damian.mizera@student.admin.pk.edu.pl>
2021-10-27 18:48:23 +0200

    fixed project structure

--- allocation_basics/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ allocation_basics/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/HelloWorld.dir/DependInfo.cmake"
+  "CMakeFiles/allocation_basics.dir/DependInfo.cmake"
   )
